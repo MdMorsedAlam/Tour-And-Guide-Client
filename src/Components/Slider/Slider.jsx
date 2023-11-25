a8401f54a40185f3678ffe3789bc922669627b92
Morsed <mdmorsedalam324@gmail.com>
2023-11-25 09:10:45 +0600

    Slider Done

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,26 +1,25 @@
-import AwesomeSlider from 'react-awesome-slider';
-import withAutoplay from 'react-awesome-slider/dist/autoplay';
-import 'react-awesome-slider/dist/styles.css';
+import AwesomeSlider from "react-awesome-slider";
+import withAutoplay from "react-awesome-slider/dist/autoplay";
+import "react-awesome-slider/dist/styles.css";
 
 const AutoplaySlider = withAutoplay(AwesomeSlider);
 
-
 const Slider = () => {
- return (
-  <div>
-    <AutoplaySlider
-    play={true}
-    cancelOnInteraction={false} // should stop playing on user interaction
-    interval={6000}
-  >
-    <div data-src="https://i.ibb.co/Mkm9VFD/educational.jpg" />
-    <div data-src="https://i.ibb.co/Mkm9VFD/educational.jpg" />
-    <div data-src="https://i.ibb.co/Mkm9VFD/educational.jpg" />
-    <div data-src="https://i.ibb.co/Mkm9VFD/educational.jpg" />
-    <div data-src="https://i.ibb.co/Mkm9VFD/educational.jpg" />
-  </AutoplaySlider>
-  </div>
- );
+  return (
+    <div className="">
+      <AutoplaySlider
+        play={true}
+        cancelOnInteraction={false} // should stop playing on user interaction
+        interval={6000}
+      >
+        <div data-src="https://i.ibb.co/Mkm9VFD/educational.jpg" />
+        <div data-src="https://i.ibb.co/Mkm9VFD/educational.jpg" />
+        <div data-src="https://i.ibb.co/Mkm9VFD/educational.jpg" />
+        <div data-src="https://i.ibb.co/Mkm9VFD/educational.jpg" />
+        <div data-src="https://i.ibb.co/Mkm9VFD/educational.jpg" />
+      </AutoplaySlider>
+    </div>
+  );
 };
 
 export default Slider;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,16 @@
-import { Helmet } from 'react-helmet-async';
-import Slider from '../../Components/Slider/Slider';
+import { Helmet } from "react-helmet-async";
+import Slider from "../../Components/Slider/Slider";
+import Tourism from "../../Components/Tourism/Tourism";
 const Home = () => {
- return (
-  <div>
-   <Helmet>
-    <title>Home | Tourist Guide</title>
-   </Helmet>
-   <Slider/>
-  </div>
- );
+  return (
+    <div>
+      <Helmet>
+        <title>Home | Tourist Guide</title>
+      </Helmet>
+      <Slider />
+      <Tourism/>
+    </div>
+  );
 };
 
 export default Home;
